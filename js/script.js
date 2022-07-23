b4d7f3013396ad69a89902b21bc43025526a5a15
Gin Gin Nurilham M <ginnncore717@gmail.com>
2022-07-23 17:48:16 +0700

    menyelesaikan tampilan website untuk versi mobile

--- js/script.js
+++ js/script.js
@@ -2,58 +2,58 @@
 const DATA_STORE = [{
         nama: 'Mie Daging Pedas',
         harga: '10.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food1.png',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Nasi Goreng',
         harga: '20.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food2.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Sushi',
         harga: '35.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food3.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Sosis Bakar',
         harga: '10.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food4.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Hamburger',
         harga: '15.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food5.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Pizza',
         harga: '25.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food6.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Sate Ayam',
         harga: '20.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food7.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
     {
         nama: 'Hot Dog',
         harga: '15.000',
-        bahan: [],
+        bahan: ['bahan-1', 'bahan-2', 'bahan-3', 'bahan-4', 'bahan-5', 'bahan-6'],
         url: 'food8.jpg',
-        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor obcaecati possimus dolores minus. Consectetur.'
+        keteranngan: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ullam harum cumque nostrum ut magni, quia vel sed nulla.'
     },
 ]
 
@@ -68,4 +68,102 @@ menus.forEach(function (menu) {
         navMenu.classList.toggle('show');
     });
 });
+
+// !menampilkan produk-produk
+function cetakProduk(item) {
+    return item.map(el => {
+        return `<div class="col-8">
+        <div class="image-product" style="background-image: url(../assets/${el.url});"></div>
+        <div class="text-product">
+            <h3>${el.nama}</h3>
+            <p>${el.harga}</p>
+            <div class="button-product">
+            <a href="" class="order button">Order</a>
+            <a href="#detail" class="detail-button button" onlick="showDetail()">Detail</a>
+            </div>
+        </div>
+    </div>`
+    }).join('')
+}
+
+const listProduk = document.querySelector('.row-product');
+listProduk.innerHTML = cetakProduk(DATA_STORE);
+
+
+// !animasi menu kategori
+const circleMenus = document.querySelectorAll('.circle');
+const dropMenu = document.querySelector('.drop-down-menu');
+
+for (let i = 0; i < circleMenus.length; i++) {
+    circleMenus[i].addEventListener('click', () => {
+        dropMenu.classList.toggle('show');
+    })
+}
+
+// !menampilkan produk sesuai kategori yang diklik user
+dropMenu.addEventListener('click', events => {
+    if (events.target.classList.contains('category-1')) return category(DATA_STORE[0], DATA_STORE[1]);
+    if (events.target.classList.contains('category-2')) return category(DATA_STORE[2], DATA_STORE[3]);
+    if (events.target.classList.contains('category-3')) return category(DATA_STORE[4], DATA_STORE[7]);
+    if (events.target.classList.contains('category-4')) return category(DATA_STORE[5]);
+    if (events.target.classList.contains('category-5')) return category(DATA_STORE[6]);
+    if (events.target.classList.contains('category-6')) {
+        dropMenu.classList.remove('show');
+        listProduk.innerHTML = cetakProduk(DATA_STORE)
+    };
+})
+
+function category(...item) {
+    dropMenu.classList.remove('show');
+    listProduk.innerHTML = item.map(el => {
+        return `<div class="col-8">
+        <div class="image-product" style="background-image: url(../assets/${el.url});"></div>
+        <div class="text-product">
+            <h3>${el.nama}</h3>
+            <p>${el.harga}</p>
+            <div class="button-product">
+            <a href="" class="order button">Order</a>
+            <a href="#detail" class="detail-button button" onlick="showDetail()">Detail</a>
+            </div>
+        </div>
+    </div>`
+    }).join('')
+}
+
+// !menampilkan detail produk sesuai yang diklik user
+function cetakDetailProduk(el) {
+        return `<div class="col-8">
+            <div class="title-detail">
+                <h3 class="name-of-food">${el.nama}</h3>
+            </div>
+            <div class="image-detail" style="background-image: url(../assets/${el.url});"></div>
+            <div class="more-detail">
+                <p>${el.keteranngan}</p>
+                <div class="ingredients">
+                    <h3>Bahan-bahan :</h3>
+                    <ul>    
+                        ${cetakBahan(el.bahan)}
+                    </ul>
+                </div>
+            </div>
+        </div>`
+}
+
+function cetakBahan(bahan) {
+    return bahan.map(el => {
+        return `<li>${el}</li>`
+    }).join('');
+}
+
+const rowDetail = document.querySelector('.row-detail');
+
+function showDetail() {
+    const detailButton = document.querySelectorAll('.detail-button');
+    for (let i = 0; i < DATA_STORE.length; i++) {
+        detailButton[i].addEventListener('click', () => {
+            console.log(i)
+            return rowDetail.innerHTML = cetakDetailProduk(DATA_STORE[i]);
+        })
+    }
+}
 
